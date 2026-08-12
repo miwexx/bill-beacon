5f9b3956443369f71c37ddffb05a809950600ff3
miwexx <87953725+miwexx@users.noreply.github.com>
2026-08-11 23:03:33 -0400

    fix

--- js/app.js
+++ js/app.js
@@ -1047,7 +1047,7 @@ window.openCalendarDay = function(dateString) {
 
       <div class="sheet-nav">
         <button class="nav-button" onclick="closeCalendarDay()">Close</button>
-        <div class="sheet-title">On This Day
+        <div class="sheet-title">Schedule
         <div style="width:54px"></div>
       </div>
 
