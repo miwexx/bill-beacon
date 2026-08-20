4b0ebe8a7aa0874e18c56962516cb04fdf88f1fe
miwexx <87953725+miwexx@users.noreply.github.com>
2026-08-19 21:04:35 -0400

    fix

--- js/app.js
+++ js/app.js
@@ -276,7 +276,7 @@ function billVisual(bill, size = 32) {
           width:${size}px;
           height:${size}px;
           object-fit:contain;
-transform:scale(1.28);
+transform:scale(1.30);
 padding:0;
 border-radius:0;
         "
