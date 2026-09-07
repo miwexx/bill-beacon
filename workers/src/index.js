4efe8e1a2f6435bf84a9fbf3e9b5a1c9762a62ae
miwexx <87953725+miwexx@users.noreply.github.com>
2026-09-07 04:01:09 +0000

    minor

--- workers/src/index.js
+++ workers/src/index.js
@@ -466,7 +466,7 @@ export default {
         await sendPushNotification(
           subscription,
           {
-            title: '${bill.name} Payment Reminder',
+            title: 'Payment Reminder',
             body: message,
             url: buildBillDeepLink(bill.id),
             billId: bill.id,
